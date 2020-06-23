508116cd29c302420d898f96075d73fdf4243c5e
Anurag Dalmia <anuragdalmiya@gmail.com>
2020-06-24 03:20:50 +0530

    added custom city weather

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
   double maxTemp;
   double minTemp;
   String iconID;
+  String inputCity;
 
   @override
   void initState() {
@@ -62,30 +63,46 @@ class _HomeState extends State<Home> {
       return Scaffold(
         appBar: AppBar(),
         body: Center(
-            child: Column(
-          children: <Widget>[
-            Text('$cityName,' + '$countryName'),
-            Text('${temperature.toInt()}°' + 'C'),
-            Text(condition.toString()),
-            Text(conditionDescription.toString()),
-            Text(conditionId.toString()),
-            Text(windSpeed.toInt().toString() + ' km/h'),
-            Text(humidity.toString() + ' %'),
-            Text('${maxTemp.toInt()}°' + 'C'),
-            Text('${minTemp.toInt()}°' + 'C'),
-            Image.network(
-              'http://openweathermap.org/img/wn/$iconID@2x.png',
-              scale: .9,
-            ),
-            FlatButton(
+          child: Column(
+            children: <Widget>[
+              Text('$cityName,' + '$countryName'),
+              Text('${temperature.toInt()}°' + 'C'),
+              Text(condition.toString()),
+              Text(conditionDescription.toString()),
+              Text(conditionId.toString()),
+              Text(windSpeed.toInt().toString() + ' km/h'),
+              Text(humidity.toString() + ' %'),
+              Text('${maxTemp.toInt()}°' + 'C'),
+              Text('${minTemp.toInt()}°' + 'C'),
+              Image.network(
+                'http://openweathermap.org/img/wn/$iconID@2x.png',
+                scale: .9,
+              ),
+              FlatButton(
                 child: Icon(Icons.my_location),
                 onPressed: () async {
                   var futureData = await weatherModel.getFutureWeather();
                   var currentData = await weatherModel.getCurrentWeather();
                   updateData(futureData, currentData);
-                })
-          ],
-        )),
+                },
+              ),
+              TextField(
+                onChanged: (value) {
+                  inputCity = value;
+                },
+              ),
+              FlatButton(
+                child: Text('Get Weather'),
+                onPressed: () async {
+                  var currentData =
+                      await weatherModel.getCityCurrentWeather(inputCity);
+                  var futureData = await weatherModel.getCityFutureWeather();
+                  updateData(futureData, currentData);
+                },
+              )
+            ],
+          ),
+        ),
       );
   }
 }
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -3,6 +3,8 @@ import 'package:clima/services/networking.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String apiKey = DotEnv().env['API_KEY'];
+double inputCityLat;
+double inputCityLong;
 
 class WeatherModel {
   Future<dynamic> getCurrentWeather() async {
@@ -34,6 +36,27 @@ class WeatherModel {
         'http://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&exclude=minutely&appid=$apiKey');
 
     var futureData = await networkHelper2.getData();
+    return futureData;
+  }
+
+  Future<dynamic> getCityCurrentWeather(String inputCity) async {
+    NetworkHelper networkHelper3 = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/weather?q=$inputCity&appid=$apiKey');
+
+    var currentData = await networkHelper3.getData();
+    inputCityLat = currentData['coord']['lat'];
+    inputCityLong = currentData['coord']['lon'];
+    return currentData;
+  }
+
+  Future<dynamic> getCityFutureWeather() async {
+    double latitude = inputCityLat;
+    double longitude = inputCityLong;
+
+    NetworkHelper networkHelper4 = NetworkHelper(
+        'http://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&exclude=minutely&appid=$apiKey');
+
+    var futureData = await networkHelper4.getData();
     return futureData;
   }
 }
